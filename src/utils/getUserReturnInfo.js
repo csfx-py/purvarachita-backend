92715eae8e36405f260816ed37e2ca9cd3585842
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-12-16 09:25:13 +0530

    taken from mentor:  extract user info from _doc

---
+++ src/utils/getUserReturnInfo.js
@@ -0,0 +1,10 @@
+module.exports = (userDoc) => {
+  return {
+    _id: userDoc._id,
+    name: userDoc.name,
+    email: userDoc.email,
+    posts: userDoc.posts,
+    avatar: userDoc.avatar,
+    role: userDoc.role,
+  };
+};
