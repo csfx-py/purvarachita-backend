2e855f416e4242f4360b1c50077cd71f03fde68d
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-12-16 09:28:44 +0530

    taken from mentor:  hooks

--- src/models/Post.js
+++ src/models/Post.js
@@ -64,4 +64,51 @@ const PostSchema = new mongoose.Schema({
   },
 });
 
+// pre delete middleware remove refs from User
+PostSchema.pre("deleteOne", async function (next) {
+  try {
+    const User = require("./User");
+    const post = await this.model.findOne(this.getQuery());
+
+    await User.updateOne({ _id: post.user }, { $pull: { posts: post._id } });
+
+    next();
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+// pre save middleware save new refs to User
+PostSchema.pre("save", async function (next) {
+  try {
+    const User = require("./User");
+    const post = this;
+
+    await User.updateOne({ _id: post.user }, { $push: { posts: post._id } });
+
+    next();
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+PostSchema.pre("deleteMany", async function (next) {
+  try {
+    const User = require("./User");
+    const posts = await this.model.find(this.getQuery());
+
+    for (let post of posts) {
+      await User.updateOne({ _id: post.user }, { $pull: { posts: post._id } });
+    }
+
+    next();
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+
 module.exports = mongoose.model("Post", PostSchema);
